b44a956311fa3192afa96df548b8c080e1d00a3b
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-25 22:04:38 +0530

    Add files via upload

---
+++ EXPERIMENT-1/EXP1Q4.py
@@ -0,0 +1,12 @@
+#WAP to input principal amount, rate of interest and interest then calculate time.
+si=1000
+p=100
+r =10
+
+t=si*100/p*r
+
+print('The Simple Interest is', si)
+print('The principal is', p)
+print('The rate of interest is',r)  
+print('The Time is',t) 
+  
