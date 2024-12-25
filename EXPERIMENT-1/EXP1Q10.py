261edb392bc08083612d4cb4369c39380f5d5368
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-25 22:11:20 +0530

    Add files via upload

---
+++ EXPERIMENT-1/EXP1Q10.py
@@ -0,0 +1,5 @@
+# WAP to enter your name in lower case and convert it into upper case and vice versa.
+
+name = input("Enter your name: ")
+print("Name in uppercase:", name.upper())
+print("Name in lowercase:", name.lower())
