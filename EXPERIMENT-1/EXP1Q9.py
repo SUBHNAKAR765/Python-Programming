78ef24a8a85e74782fff09ad1c586ac9cd68f4c8
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-25 22:07:08 +0530

    Add files via upload

---
+++ EXPERIMENT-1/EXP1Q9.py
@@ -0,0 +1,6 @@
+x = int(input("Enter value of x (2-8): "))
+y = int(input("Enter value of y (2-8): "))
+n = int(input("Enter value of n (2-8): "))
+
+result = (((x ** 2 + x ** 3) / (y/4 + y/3 + y **8 )) **(2*n))*((y**6 + y**2)/(x**9))
+print("Result of the equation:", result)
