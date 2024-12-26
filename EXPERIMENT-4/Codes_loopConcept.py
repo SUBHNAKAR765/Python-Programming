91eda5eb5e6514a093d6f11d2edf52b4d8bd881b
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-26 10:52:45 +0530

    Add files via upload

---
+++ EXPERIMENT-4/Codes_loopConcept.py
@@ -0,0 +1,30 @@
+#WAP in python to print the number from 1-10 using while loop?
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+#WAP in python to print the even numbers between 1 to 50 using while loop?
+
+i = 2
+while i <= 50:
+    print(i)
+    i += 2
+
+#WAP in python to print those numbers which are divisible either 50r 7 within 1 to 50 using while loop?
+
+i = 1
+while i <= 50:
+    if i % 5 == 0 or i % 7 == 0:
+        print(i)
+    i += 1
+
+#WAP in python to print the sum of 5 natural numners ?
+
+i = 1
+sum = 0
+while i <= 5:
+    sum += i
+    i += 1
+print(sum)
