30da36417f460e4a422bc87ce4c06a0895f0c82b
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-27 10:35:33 +0530

    Add files via upload

---
+++ EXPERIMENT-7/EXP7Q2.py
@@ -0,0 +1,8 @@
+# WAP in python to demonstrate the use of dir () method using module.
+
+# mymodule.py
+def f1():
+    pass
+
+def f2():
+    pass
---
+++ EXPERIMENT-7/EXP7Q5.py
@@ -0,0 +1,9 @@
+# WAP in python to demonstrate name clashes in different name spaces
+
+# module1.py
+def f():
+    print("Module 1")
+
+# module2.py
+def f():
+    print("Module 2")
